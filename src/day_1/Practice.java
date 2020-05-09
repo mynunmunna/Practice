ff68459d0a11f5de113d2818b2329ae86b55e756
mynunmunna <mynun.munna@gmail.com>
2020-05-09 18:49:39 -0400

    add Practice.java

---
+++ src/day_1/Practice.java
@@ -0,0 +1,13 @@
+package day_1;
+
+public class Practice {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+        System.out.println("Practicing git today");
+
+
+
+
+    }
+}
